6872c7228d5d96db6681377399e23ebd23ca9271
ablanchiet <73057246+ablanchiet@users.noreply.github.com>
2021-01-29 13:15:30 +0100

    mouvement ennemi

--- la_chasse.cpp
+++ la_chasse.cpp
@@ -1,14 +1,15 @@
+#include "classe_globale.cpp"
 int la_chasse(Hero hero, Enemy ennemi){
-    int x=hero.pos_x - ennemi.pos_x;
-    int y=hero.pos_y - ennemi.posy_y;
+    int x=hero.Pos_x - ennemi.Pos_x;
+    int y=hero.Pos_y - ennemi.Pos_y;
     if (x*x>y*y){
         if (x<0){return 4;}
-        else {return 6;}
+        else {return 5;}
 
     }
     else {
-        if (y<0){return 2;}
-        else{return 8;} 
+        if (y<0){return 3;}
+        else{return 2;} 
     }
 
 }
