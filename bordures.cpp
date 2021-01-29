6dad0f45c2df9419c06f832c3b13ce1fd34b67f5
FontaineF <florestan.fontaine.com>
2021-01-29 16:46:50 +0100

    bordures, le retour

--- bordures.cpp
+++ bordures.cpp
@@ -6,8 +6,10 @@ int window_top = 3;
 char map[17][98];
 for(int i = 0; i<17; i++){
     for(int j = 0; j<98; j++){
-        map[i][j] = ' '
+        map[i][j] = ' ';
     }
 }
 
-for(int k = 5
+for(int k = 5; k<35; k++){
+    map[18][k] = '|';
+}
